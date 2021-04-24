032073949fd1ca705003f7e4dcec3d988c3786fa
Edb83 <ed3radley@gmail.com>
2021-04-24 21:17:01 +0000

    Add static file configuration

--- moose_juice/settings.py
+++ moose_juice/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-            
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -151,6 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
