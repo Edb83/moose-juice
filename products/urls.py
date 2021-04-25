7311da9dc61e8a519271dee024067c5f900c375b
Edb83 <ed3radley@gmail.com>
2021-04-25 20:26:43 +0000

    Add Products view and template

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,6 @@
-# from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.all_products, name='products'),
 ]
