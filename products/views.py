100fb9e78e679954f48f336bf802a57af735955a
Edb83 <ed3radley@gmail.com>
2021-04-28 23:05:00 +0000

    Add sort selection and JS to products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q  # for handling complex queries
+from django.db.models.functions import Lower
 from .models import Product, Brand, Category
 
 
@@ -11,8 +12,23 @@ def all_products(request):
     brand = None
     query = None
     category = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'brand' in request.GET:
             brand = request.GET['brand']
             products = products.filter(brand__name=brand)
@@ -29,16 +45,23 @@ def all_products(request):
                 messages.error(request, "You forgot to put the words in!")
                 return redirect(reverse('products'))
             # Need to amend to search on flavours
-            # Q(tags__name__icontains=query)
+
             queries = (
-                Q(friendly_name__icontains=query) | Q(description__icontains=query))
+                Q(friendly_name__iexact=query) |
+                # Q(description__icontains=query)
+                Q(tags__name__iexact=query)
+                )
+
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'brand': brand,
         'category': category,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
