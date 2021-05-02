4cec8a30596d60043406c44481063ea669ca36ef
Edb83 <ed3radley@gmail.com>
2021-05-02 11:40:23 +0000

    Add add_to_cart functionality inc size and nic

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def cart_contents(request):
 
-    cart_items = [1]
+    cart_items = []
     total = 0
     product_count = 0
 
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view to return the shopping cart contents """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the chosen product (inc options) to the shopping cart """
+
+    size = request.POST.get('size')
+    nic = request.POST.get('nic')
+    item = f'{item_id}_{size}_{nic}'
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item in list(bag.keys()):
+        bag[item] += quantity
+    else:
+        bag[item] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
