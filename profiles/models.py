5807c8cdfc2338372bfa10eb78294533f62b0a63
Edb83 <ed3radley@gmail.com>
2021-05-06 11:28:33 +0000

    Add profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -17,12 +17,12 @@ class UserProfile(models.Model):
 
     # Default delivery details
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     # Favourite products
     favourites = models.ManyToManyField(Product)
