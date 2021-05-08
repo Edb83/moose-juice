42a34885751ea7ab2e586858999fa499619ef0e8
Edb83 <ed3radley@gmail.com>
2021-05-08 14:22:31 +0000

    Add confirmation email facility (not hooked up yet)

--- moose_juice/settings.py
+++ moose_juice/settings.py
@@ -186,3 +186,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'moosejuice@example.com'
