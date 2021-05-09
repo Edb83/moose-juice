dce156e0532789a8f193f2e8e4a3514398291cd5
Edb83 <ed3radley@gmail.com>
2021-05-09 20:14:01 +0000

    Add edit product functionality

--- TESTING.md
+++ TESTING.md
@@ -10,3 +10,8 @@ Solution: due to setting default=0 ?
 <!-- Bug 4: line order items have price tied to the model when it should be set on save
 Solution:  -->
 
+Bug 5: custom save method on Review model used to request average_rating calculation on Product model. Rating was calculated before the new rating was saved meaning inaccurate ratings
+Solution: use a signal on Review creation/update/delete to call calculate_rating
+
+Bug 6: review save/delete signal would not fire as expected
+Solution: import the signal in the product/apps.py
--- products/views.py
+++ products/views.py
@@ -107,3 +107,32 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """
+    Edit a product in the store
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated {product.name}')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product - please check form and try again')
+    
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit-product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
