0ff5134c78e74ac1ce8a3884a303f49095849c0e
Edb83 <ed3radley@gmail.com>
2021-05-12 16:17:40 +0000

    Fix typo in gmail integration (remote rather than smtp)

--- moose_juice/settings.py
+++ moose_juice/settings.py
@@ -227,7 +227,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'moosejuice@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
