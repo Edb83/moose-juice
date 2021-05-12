289758a7d1668ace9cceba7f915a5c690fd42d28
Edb83 <ed3radley@gmail.com>
2021-05-12 12:39:54 +0000

    Install gunicorn and add Procfile ready for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn moose-juice.wsgi:application
--- moose_juice/settings.py
+++ moose_juice/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moose-juice.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,16 +126,21 @@ WSGI_APPLICATION = 'moose_juice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# DATABASES = {
-#     'default': dj_database_url.parse('postgres://<INSERT URL HERE>')
-# }
+DATABASES = {
+    'default': dj_database_url.parse('postgres://<INSERT URL HERE>')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
