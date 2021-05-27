d5eb6791ee305b793fa0051b98abe07388b4e14c
Edb83 <ed3radley@gmail.com>
2021-05-27 13:35:13 +0000

    Move inline scripts to static folders; change checkout quantities to update on caret click

---
+++ cart/static/cart/js/remove-item.js
@@ -0,0 +1,16 @@
+$('.remove-item').click(function(e) {
+
+    // As remove button is outside quantity form we need to
+    // find an object with matching item_id where the csrf_token
+    // has been added to its data-item
+    let itemId = $(this).attr('id').split('remove_')[1];
+    let nearbyTokenHolder = $(`#id_qty_${itemId}`)
+    let csrfToken = nearbyTokenHolder.data('token');
+    let url = `/cart/remove/${itemId}/`;
+    let data = {'csrfmiddlewaretoken': csrfToken};
+
+    $.post(url, data)
+     .done(function() {
+         location.reload();
+     });
+})
---
+++ static/js/quantity-input.js
@@ -0,0 +1,44 @@
+// Disable +/- buttons outside 1-99 range
+function handleEnableDisable(itemId) {
+    let currentValue = parseInt($(`#id_qty_${itemId}`).val());
+    let minusDisabled = currentValue < 1;
+    let plusDisabled = currentValue > 98;
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+// Ensure proper enabling/disabling of all inputs on page load
+let allQtyInputs = $('.qty_input');
+for(let i = 0; i < allQtyInputs.length; i++){
+    let itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+
+// Check enable/disable every time the input is changed
+$('.qty_input').change(function() {
+    let itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// Increment quantity
+$('.increment-qty').click(function(e) {
+   let closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   let currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue + 1);
+   let itemId = $(this).data('item_id');
+   handleEnableDisable(itemId);
+});
+
+// Decrement quantity
+$('.decrement-qty').click(function(e) {
+   let closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   let currentValue = parseInt($(closestInput).val());
+   if (currentValue - 1 == 0) {
+    $(closestInput).val = 1
+    e.preventDefault();
+   } else {
+       $(closestInput).val(currentValue - 1);
+   }
+   let itemId = $(this).data('item_id');
+   handleEnableDisable(itemId);
+});
