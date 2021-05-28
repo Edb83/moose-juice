69f8d7eb0acab919342e1c20700981c1d55a98ac
Edb83 <ed3radley@gmail.com>
2021-05-28 23:17:34 +0000

    Add svg colour picker to Add_Product

---
+++ products/static/products/js/change-color.js
@@ -0,0 +1,9 @@
+let colorPicker = $('#id_color');
+let colorGradient = document.querySelector('.js-stopColor');
+let bottleFill = document.querySelector('.js-bottle');
+let chosenColor;
+$(colorPicker).on('input',function () {
+    chosenColor = this.dataset.currentColor;
+        colorGradient.setAttribute('stop-color', chosenColor)
+        bottleFill.setAttribute('fill', "url('#-gradient')")
+})
