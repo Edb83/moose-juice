2734893b1a75fc3307f9873903c0c891800ae137
Edb83 <ed3radley@gmail.com>
2021-05-29 01:14:34 +0000

    Add example juice card to add/edit product inc text mirror

--- products/static/products/js/change-color.js
+++ products/static/products/js/change-color.js
@@ -2,6 +2,7 @@ let colorPicker = $('#id_color');
 let colorGradient = document.querySelector('.js-stopColor');
 let bottleFill = document.querySelector('.js-bottle');
 let chosenColor;
+
 $(colorPicker).on('input',function () {
     chosenColor = this.dataset.currentColor;
         colorGradient.setAttribute('stop-color', chosenColor)
---
+++ products/static/products/js/input-mirror.js
@@ -0,0 +1,17 @@
+// http://jsfiddle.net/6khr8e2b/
+
+$(function(){
+    let nameInput = $('#id_name');
+    let nameOutput = $('.card-title');
+    function onChange() {
+        if (nameInput.val()) {
+            nameOutput.text(nameInput.val());
+        } else {
+            nameOutput.text("?")
+        }
+    };
+
+    $(nameInput)
+        .change(onChange)
+        .keyup(onChange);
+});
