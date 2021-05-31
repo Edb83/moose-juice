e343e8af0a665ded181357751c1b7c49fcd5b614
Edb83 <ed3radley@gmail.com>
2021-05-31 21:25:12 +0000

    Fix W3C errors

---
+++ home/static/home/js/hero-blur.js
@@ -0,0 +1,2 @@
+$('.hero-overlay').fadeTo(4000, 0);
+$('.hero-text-container').fadeTo(2500, 1);
--- products/urls.py
+++ products/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
         'edit_review/<int:review_id>/',
         views.edit_review,
         name='edit_review'),
-    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
+    path(
+        'delete_review/<int:review_id>/',
+        views.delete_review, name='delete_review'),
 ]
