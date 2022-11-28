bafd71ed955da00bdf71aea1768849aef8016162
Edb83 <ed3radley@gmail.com>
2022-11-28 14:24:47 +0000

    Add build.sh and update settings.py for Render deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn moose_juice.wsgi:application
--- moose_juice/settings.py
+++ moose_juice/settings.py
@@ -31,6 +31,9 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['moose-juice.herokuapp.com', 'localhost']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
